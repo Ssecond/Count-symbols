693a2351e7678c5fda18babf9dd41bc6570d82b6
Artem <artemtrushnikov@mail.ru>
2021-10-26 21:42:05 +1000

    Добавьте файлы проекта.

---
+++ Задача 3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = string.Empty;
+            s = Console.ReadLine();
+            char c = '(';
+            int z = 0;
+            for (int x = 0;x < s.Length;x++)
+            {
+                if (s[x] == c)
+                {
+                    z++;
+                }
+            }
+            Console.WriteLine(z);
+            Console.ReadLine();
+        }
+    }
+}
